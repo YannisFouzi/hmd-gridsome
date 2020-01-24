89ee8b8e2d6eff22b2b3244d13e9a3cf437d331b
Yannis Fouzi <56967021+YannisFouzi@users.noreply.github.com>
2020-01-24 14:24:22 +0100

    feat: add page contact

---
+++ src/pages/Contact.vue
@@ -0,0 +1,75 @@
+<template>
+  <Layout>
+    <div class="container has-text-centered">
+      <section class="section">
+        <div class="container">
+          <h2 class="title is-2">Nous contacter</h2>
+          <div class="columns is-vcentered">
+            <div class="column is-6">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2625.9748343557426!2d2.3536474166425614!3d48.83961870998068!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e671f23f93a231%3A0xe3ec6467503ce0a8!2s4%20Rue%20du%20Fer%20%C3%80%20Moulin%2C%2075005%20Paris!5e0!3m2!1sfr!2sfr!4v1579778241706!5m2!1sfr!2sfr"
+                style="height: 500px"
+                allowfullscreen
+                width="800"
+              ></iframe>
+            </div>
+            <div class="column is-6">
+              <div>
+                <div class="media">
+                  <div class="media-left">
+                    <b-icon pack="far" icon="envelope" size="is-medium" />
+                  </div>
+                  <div class="media-content">
+                    <div class="content">
+                      <a href="mailto:y.fouzidu95@gmail.com">hamidou.abou@hmd-expertise.fr</a>
+                    </div>
+                  </div>
+                </div>
+                <div class="media">
+                  <div class="media-left">
+                    <b-icon pack="fas" icon="phone-alt
+" size="is-medium" />
+                  </div>
+                  <div class="media-content">
+                    <div class="content">
+                      <p>06 21 71 54 37</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="media">
+                  <div class="media-left">
+                    <b-icon pack="fas" icon="map-marker-alt
+" size="is-medium" />
+                  </div>
+                  <div class="media-content">
+                    <div class="content">
+                      <p>4, rue du Fer à Moulin 75005 Paris</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="media">
+                  <div class="media-left">
+                    <b-icon pack="far" icon="clock" size="is-medium" />
+                  </div>
+                  <div class="media-content">
+                    <div class="content">
+                      <p>Matin : 9H - 12H30, Après-midi : 13H30 - 18H</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Contact"
+  }
+};
+</script>
